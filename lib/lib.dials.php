90bdc46619e37232fc887bce2a8aae32943b162e
Tom Kisha <x0rsw1tch@gmail.com>
2015-04-26 22:58:18 -0400

    Added construct to auto-populate required objects.... Added getDials()  method to retrieve user dials

--- lib/lib.dials.php
+++ lib/lib.dials.php
@@ -15,16 +15,20 @@ class Dials {
     public $dials;
 
 
-
     function __construct (Categories $categories) {
-
-
+        $this->categories = $categories;
+        $this->db = $categories->db;
+        $this->user = $categories->user;
     }
 
 
-    public function getDials ($userId) {
-
-
+    public function getDials () {
+        $sql = 'SELECT * FROM spd_dials WHERE user = :user ORDER BY `order` ASC';
+        $params = Array(
+            Array('param' => ':user', 'value' => $this->user->userId, 'type' => PDO::PARAM_STR),
+        );
+        $r = $this->db->execQuery($this->db, $sql, $params);
+        $this->dials = $r->fetchAll();
 
     }
 
