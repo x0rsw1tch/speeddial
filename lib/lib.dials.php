65c384443a825d73f270a60f8510b74794009de6
Tom Kisha <x0rsw1tch@gmail.com>
2015-04-26 22:30:45 -0400

    new file: Added dials class

---
+++ lib/lib.dials.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tom
+ * Date: 026 4/26/2015
+ * Time: 10:28 PM
+ */
+
+
+class Dials {
+
+    public $db;
+    public $user;
+    public $categories;
+    public $dials;
+
+
+
+    function __construct (Categories $categories) {
+
+
+    }
+
+
+    public function getDials ($userId) {
+
+
+
+    }
+
+
+
+}
+
+
+
+?>
